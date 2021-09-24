1e949fdf7a9d876059d71267650194f05dc373ea
davidczepiel <48888283+davidczepiel@users.noreply.github.com>
2021-09-24 11:55:32 +0200

    Se muestra el coste de la funcion en una grafica 3D

--- Practica1/Source.py
+++ Practica1/Source.py
@@ -1,4 +1,7 @@
 from matplotlib import pyplot as plt
+from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import numpy as np
 from pandas.io.parsers import read_csv
 
@@ -12,8 +15,8 @@ def main():
     X = valores[:, 0]
     Y = valores[:, 1]
     #valores iniciales de theta0 y theta1
-    theta0 = 157
-    theta1 = 244
+    theta0 = 0
+    theta1 = 0
     
     #aplicamos el descanso de gradiente con NUM_ITERATIONS iteraciones
     for i in range(NUM_ITERATIONS):
@@ -26,8 +29,47 @@ def main():
         theta0 = theta0 - ((ALPHA/len(X)) * sum0)
         theta1 = theta1 - ((ALPHA/len(X)) * sum1)
     
-    #dibujamos la funcion
-    drawFunction(X, Y, theta0, theta1)
+
+    #Preparamos los datos que vamos a necesitar para a representar la el coste en una grafica 3D
+    Theta0,Theta1,Coste = make_data(np.array([-10,10]),np.array([-1,4]),X,Y)
+
+    #Preparamos la grafica
+    fig = plt.figure()
+    ax = fig.gca(projection = '3d')
+    surf = ax.plot_surface(Theta0, Theta1, Coste, cmap=cm.coolwarm,
+                       linewidth=0, antialiased=False)
+
+    #Sacamos los valores minimo y maximo que vamos a representar en la misma
+    min = np.min(Coste)
+    max = np.max(Coste)
+    ax.set_zlim(min,max)
+    ax.zaxis.set_major_locator(LinearLocator(10))
+    ax.zaxis.set_major_formatter('{x:.02f}')
+
+    # Add a color bar which maps values to colors.
+    fig.colorbar(surf, shrink=0.5, aspect=5)
+    #Mostramos la grafica 3d del coste de la funcion
+    plt.show()  
+
+
+def coste(X,Y,Theta0,Theta1):
+    costeTotal=0
+    for i in range(len(X)):
+        valor = Theta0 + (Theta1*X[i])
+        costeTotal += ((valor-Y[i])**2)
+    
+    return costeTotal/(2*len(X))
+
+
+def make_data ( t0_range , t1_range , X , Y ) :
+    step = 0.1
+    Theta0 = np.arange(t0_range[ 0 ] , t0_range[ 1 ] , step )
+    Theta1 = np.arange( t1_range [ 0 ] , t1_range[ 1 ] , step )
+    Theta0 , Theta1 = np.meshgrid(Theta0 , Theta1 )
+    Coste = np.empty_like( Theta0 )
+    for ix , iy in np.ndindex ( Theta0.shape ) :
+        Coste [ ix , iy ] = coste(X, Y , Theta0 [ ix , iy ] , Theta1 [ ix , iy ])
+    return [ Theta0 , Theta1 , Coste ]
 
 
 def drawFunction(X, Y, theta0, theta1):
