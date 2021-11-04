4da1d9d3be1a14ab7974c94bd06a8daec9ce5e61
davidczepiel <48888283+davidczepiel@users.noreply.github.com>
2021-11-04 11:57:04 +0100

    Practica 3

--- Practica3/Source.py
+++ Practica3/Source.py
@@ -13,14 +13,17 @@ from scipy.io import loadmat
 LAMBDA = 0.1
 
 def main():
-    #sacamos datos
+    parte1()
+    #parte2()
+
+
+def parte1():
+    #Sacamos los datos de las imagenes y el número que representan
     data = loadmat('ex3data1.mat')
-    #se pueden consultar las claves con data.keys()
     y = data['y']
     X = data['X']
     m = np.shape(X)[0]
     X = np.hstack([np.ones([m, 1] ) , X ] )
-    #almacena los datos leídos en X,y
 
     #Selecciona aleatoriamente 10 ejemplos y los pinta
     #sample = np.random.choice(X.shape[0], 10)
@@ -28,35 +31,95 @@ def main():
     #plt.axis('off')
     #plt.show()
 
+    #Obtenemos las thetas que predicen un numero concreto
     thetasMat = oneVsAll(X, y, 10, LAMBDA)
+    #Con estas thetas vemos cuandos valores que predecimos son iguales al valor real
     calcularAciertos(X, y, 10, thetasMat)
+
+
+def parte2():
+    #Sacamos los datos con los que vamos a trabajar
+    data = loadmat('ex3data1.mat')
+    y = data['y']
+    X = data['X']
+    m = np.shape(X)[0]
+
+    #sacamos los datos correspondientes a las thetas para la red neuronal
+    weights = loadmat ( 'ex3weights.mat' )
+    theta1 , theta2 = weights['Theta1' ] , weights['Theta2']
+    
+
+    #Le ofrecemos a la red neuronal cada uno de nuestros casos de prueba y contamos 
+    #los aciertos para sacar el porcentaje de aciertos 
+    numAciertos = 0
+    for a in range(m):
+        #Probabilidades de que una imagen concreta se corresponda a cada uno de los numeros del 0 al 9
+        probabilidades = propAdelante(X[a], theta1,theta2)
+        # Obtememos el indice del quien ha hecho una prediccion con mayor seguridad
+        # Pero a este indice le debemos sumar un 1 porque los indices van de [0,9]
+        # Y en nuestros datos tenemos valores entre [1,10]
+        prediccion = np.argmax(probabilidades, axis=0) + 1
+        if prediccion == y[a]: 
+            numAciertos +=1
+
+    print("Precision de prediccion de la red neuronal:", (numAciertos/m)*100, "%" )
+
+
+
+
+def propAdelante(X, Theta1, Theta2):
+    #Añadimos una columna de 1 para obtener a(1)
+    X = np.hstack([1, X ]) #a(1)
+
+    #Calculamos a(2) y luego le añadimos la columna de 1
+    result = sigmoid(np.matmul(Theta1, np.transpose(X))) 
+    result = np.hstack([1 , result ] ) #a(2)
+
+    #Hacemos lo mismo para a(3)
+    result = sigmoid(np.matmul(Theta2, np.transpose(result))) #a(3)
+    return result
+
+
+
+
 
 def oneVsAll(X, y, num_etiquetas, reg):
 
+    #Empezamos con las thetas sin valor
     thetasMat = np.zeros((num_etiquetas, np.shape(X)[1]))
-
     for i in range(num_etiquetas):
+        #Para cada una de las etiquetas me quedo solo con aquellas Ys que
+        #que se corresponden con la etiqueta que estoy analizando
         if i==0:
-            auxY = (y==10)*1
+            auxY = (y==10)*1 #Para la etiqueta hacemos la conversion con 10
         else:
             auxY = (y==i)*1
 
+        #Desplegamos Y porque si no hay problemas con las dimensiones de las matrices
         auxY = np.ravel(auxY)
         thetas = np.zeros(np.shape(X)[1]) ##Tantas zetas como atributos haya (deberian ser 28)
-        result = opt.fmin_tnc(func=costWithRegulation, x0=thetas, fprime=gradientWithRegulation, args=(X, auxY, reg))
 
+        #Nos guardamos las thetas que predicen la etiqueta que estamos analizando actualmente
+        result = opt.fmin_tnc(func=costWithRegulation, x0=thetas, fprime=gradientWithRegulation, args=(X, auxY, reg))
         thetasMat[i] = result[0]
     
     return thetasMat
 
 def calcularAciertos(X, y, num_etiquetas, thetasMat):    
+    #Sacamos la probabilidad de que cada conjunto de thetas 
+    #Prediga cada uno de los casos de prueba
     probabilidades = sigmoid(np.matmul(thetasMat, np.transpose(X)))
+    #Nos quedamos con aquel que este mas seguro
     prediccion = np.argmax(probabilidades, axis=0)
+    #Hacemos la conversion de aquellos que representen 0
     prediccion = np.where(prediccion == 0, 10, prediccion)
 
+    #Restamos nuestra prediccion con el valor real de cada caso
+    #de esta forma aquellos casos en los que hayamos acertado el resultado
+    #de la resta dara 0 
     aciertos = prediccion - np.ravel(y)
+    #Contamos los 0 que hay tras la resta y sacamos el porcentaje de aciertos
     numCorrectos = len(aciertos) - np.count_nonzero(aciertos)
-    #Porcentaje de predicciones acertadas
     numCorrectos = numCorrectos /len(y)*100
     print("Globalmente el porcentaje es = ", numCorrectos , "%")
 
