b0de468bc69ce096f86ea938ed8eead752b477f7
nicoFdez <nicolafe@ucm.es>
2021-11-11 17:59:26 +0100

    Sigo sin creermelo
    Co-Authored-By: davidczepiel <48888283+davidczepiel@users.noreply.github.com>

--- Practica4/Source.py
+++ Practica4/Source.py
@@ -8,13 +8,13 @@ import scipy.optimize as opt
 from sklearn.preprocessing import PolynomialFeatures
 
 from scipy.io import loadmat
+from checkNNGradients import checkNNGradients, computeNumericalGradient
 
 from displayData import displayData
 
 LAMBDA = 1
 
 def main():
-
     parte2()
 
 
@@ -30,7 +30,13 @@ def parte1():
 
     #X = np.hstack([np.ones([m, 1] ) , X ] )
 
-    print(costeRedNeuronalRegularizado(theta1, theta2, X, y, 10, LAMBDA))
+    Y = (y - 1)
+    m = len(Y)
+    y_onehot = np.zeros((m, 10))  # 5000 x 10
+    for i in range(m):
+        y_onehot[i][Y[i]] = 1
+
+    print(costeRedNeuronalRegularizado(theta1, theta2, X, y_onehot, 10, LAMBDA))
 
 
 
@@ -45,28 +51,30 @@ def parte2():
     weights = loadmat ( 'ex4weights.mat' )
     theta1 , theta2 = weights['Theta1' ] , weights['Theta2']
     
-    data1 = np.ravel(theta1)
-    data2 = np.ravel(theta2)
-    datos = np.concatenate((np.ravel(theta1), np.ravel(theta2)), None)
-    backprop(datos,m,25,10,X,y,LAMBDA)
+    #theta1 = np.hstack([np.ones([np.shape(theta1)[0], 1] ) , theta1 ] )
+    #theta2 = np.hstack([np.ones([np.shape(theta2)[0], 1] ) , theta2 ] )
+
+    datos = np.concatenate((np.ravel(theta1), np.ravel(theta2)))
+    #gradient = backprop(datos,400,25,10,X,y,LAMBDA)
+    #gradientBien = computeNumericalGradient()
+
+    print(checkNNGradients(backprop, LAMBDA))
     
-
-
 
 #GRADIENT REDES NEURONALES
 def backprop ( params_rn , num_entradas , num_ocultas , num_etiquetas , X, y , reg ):
     #Reconstruyo los datos a partir de params_rn
-    Theta1 = np.reshape( params_rn [ :num_ocultas*(num_entradas+1)] , (num_ocultas ,( num_entradas + 1)))
-    Theta2 = np.reshape( params_rn [ num_ocultas*( num_entradas + 1 ): ] , (num_etiquetas,( num_ocultas + 1 )))
+    Theta1 = np.reshape( params_rn [ :num_ocultas*(num_entradas + 1)] , (num_ocultas ,( num_entradas + 1)))
+    Theta2 = np.reshape( params_rn [ num_ocultas*(num_entradas + 1): ] , (num_etiquetas,( num_ocultas + 1 )))
     #Matrices que acumulan el gradiente
     Delta1 = np.zeros_like(Theta1)
-    Delta2 =np.zeros_like(Theta2)
+    Delta2 = np.zeros_like(Theta2)
 
     #Calculamos la salida para empezar a pensar en el gradiente
     outPut, A1, A2 = propAdelante(X,Theta1, Theta2)
-
+    m = X.shape[0]
     #BackWardPropagation
-    for t in range(X.shape(0)):
+    for t in range(m):
         #Nos quedamos con los datos correspondientes a uno de los casos
         a1t = A1[t, :] # (401,)
         a2t = A2[t, :] # (26,)
@@ -79,26 +87,22 @@ def backprop ( params_rn , num_entradas , num_ocultas , num_etiquetas , X, y , r
         Delta1 = Delta1 + np.dot(d2t[1:, np.newaxis], a1t[np.newaxis, :])
         Delta2 = Delta2 + np.dot(d3t[:, np.newaxis], a2t[np.newaxis, :])
     #Dicidir el gradiente
-    Delta1 = Delta1/X.shape(0)
-    Delta2 = Delta2/X.shape(0)
+    Delta1 = Delta1/m
+    Delta2 = Delta2/m
+
+    DeltaReg1 = Delta1[:,1:] + (Theta1[:,1:]*(reg/m))
+    DeltaReg2 = Delta2[:,1:] + (Theta2[:,1:]*(reg/m))
 
     #Calculo del coste MAL NOSE
-    cost =costeRedNeuronal(X,Theta1,Theta2)
-    gradient =0
+    cost = costeRedNeuronalRegularizado(Theta1,Theta2, X, y, num_etiquetas, reg)
+    gradient = np.concatenate((np.ravel(DeltaReg1), np.ravel(DeltaReg2)), None)
 
-    return cost,gradient
-
-# backprop devuelve una tupla ( coste , gradiente ) con el coste y el gradiente de
-# una red n e u r o n al de t r e s capas , con num_entradas , num_ocultas nodos en l a capa
-# o c ul t a y num_etiquetas nodos en l a capa de s a l i d a . Si m e s e l número de e j em pl o s
-# de en t ren amien t o , l a dimen si ón de ’X’ e s (m, num_entradas ) y l a de ’ y ’ e s
-# (m, num_etiquetas )
-
+    return cost, gradient
 
 
 def propAdelante(X, Theta1, Theta2):
     #Añadimos una columna de 1 para obtener a(1)
-    m = len(X)
+    m = X.shape[0]
     a1 = np.hstack([np.ones([m, 1]) , X ] ) #a(1)
 
     #Calculamos a(2) y luego le añadimos la columna de 1
@@ -139,22 +143,18 @@ def sigmoid(z):
 ##############################################COST####################################
 def costeRedNeuronalRegularizado(theta1, theta2, X, Y, num_labels, reg):
     m = len(X)
-    extra = (reg/(2*m)) * (np.sum(theta1**2) + np.sum(theta2**2))
+    extra = (reg/(2*m)) * (np.sum(theta1[:,1:]**2) + np.sum(theta2[:,1:]**2))
     return costeRedNeuronal(theta1, theta2, X, Y, num_labels) + extra
 
 def costeRedNeuronal(theta1, theta2, X, Y, num_labels):
-    Y = (Y - 1)
-    m = len(Y)
-    y_onehot = np.zeros((m, num_labels))  # 5000 x 10
-    for i in range(m):
-        y_onehot[i][Y[i]] = 1
-
-    h = propAdelante(X, theta1, theta2)
-    return (-1/m) * (np.sum(y_onehot * np.log(h) + (1-y_onehot) * np.log(1-h + 1e-6)))
+    h, a1, a2 = propAdelante(X, theta1, theta2)
+    m = X.shape[0]
+    return (-1/m) * (np.sum(Y * np.log(h + 1e-9) + (1-Y) * np.log(1-h + 1e-9)))
 
 
 
 ##############################################GRADIENT####################################
+
 def gradientWithRegulation(thetas, X, Y, reg):
     m = len(X)
     ajuste = (reg/m)*thetas
