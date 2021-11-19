e421ab48f2ffd45cc2bac618855c39cf66086b39
davidczepiel <48888283+davidczepiel@users.noreply.github.com>
2021-11-19 23:04:49 +0100

    Practica 5 parte 2

--- Practica5/Source.py
+++ Practica5/Source.py
@@ -6,11 +6,11 @@ from matplotlib import pyplot as plt
 LAMBDA = 1
 
 def main():
-    parte1()
+    parte2()
 
 
 def parte1():
-
+    #Obtenemos los datos con los que vamos a trabajar
     data = loadmat('ex5data1.mat')
     y = data['y']
     X = data['X']
@@ -19,30 +19,74 @@ def parte1():
     yTest = data['ytest']
     XTest = data['Xtest']
 
+    #Preparamos las thetas 
     plt.plot(X, y, "x")
-
     m = np.shape(X)[0]
     X = np.hstack([np.ones([m, 1] ) , X ] )
     n = np.shape(X)[1]
     thetas = np.zeros(n)
 
+    #Le pedimos a minimize que nos calcule las thetas que minimizan el coste por nosotros
     fmin = opt.minimize(fun=costGradientReg, x0=thetas, args=(X, y , 0), 
                         method='TNC', jac=True, options={'maxiter': 70})
-
     thetas = fmin.x
 
+    #Dibujamos la recta que hemos obtenido a partir de nuestras thetas
     minX = min(X[:,1:])
     maxX = max(X[:,1:])
     minY = thetas[0] + (thetas[1] * minX)
     maxY = thetas[0] + (thetas[1] * maxX)
     plt.plot([minX, maxX], [minY, maxY])
-
     plt.show()
+
+
+def parte2():
+    #Sacamos los datos del fichero
+    data = loadmat('ex5data1.mat')
+    y = data['y']
+    X = data['X']
+    yVal = data['yval']
+    xVal = data['Xval']
+    yTest = data['ytest']
+    XTest = data['Xtest']
+
+    #Preparamos los arrays en los que vamos a almacenar los costes 
+    m = np.shape(X)[0]
+    costesEntrenamiento = np.array([])
+    costesValidacion = np.array([])
+
+    #Preparo las x de validacion para que mas tarde a la hora de calcular
+    #Costes no haya problemas de dimensiones
+    xVal =  np.hstack([np.ones([np.shape(xVal)[0], 1] ) ,xVal] )
+
+    #Vamos a hacer m iteraciones en para calcular 
+    #los costes de los subgrupos y de los sets de validacion
+    length = range(1,m)
+    for i in length:
+        #Obtengo un nuevo set
+        Xentrenar = X[:i]
+        Yentrenar = y[:i]
+        #Entreno el algoritmo con dicho set
+        Xentrenar = np.hstack([np.ones([np.shape(Xentrenar)[0], 1] ) ,Xentrenar] )
+        thetas = np.zeros(np.shape(Xentrenar)[1])
+        fmin = opt.minimize(fun=costGradientReg, x0=thetas, args=(Xentrenar, Yentrenar , 0), 
+                            method='TNC', jac=True, options={'maxiter': 70})
+        thetas = fmin.x
+
+        #saco los 2 costes, del set y de las X de validacion
+        coste, gradiente = costGradientReg(thetas, Xentrenar, Yentrenar, LAMBDA)
+        costesEntrenamiento = np.append(costesEntrenamiento, coste)
+        coste, gradiente = costGradientReg(thetas, xVal, yVal, LAMBDA)
+        costesValidacion = np.append(costesValidacion, coste)
+
+    plt.plot(range(m-1), costesEntrenamiento)
+    plt.plot(range(m-1), costesValidacion)
+    plt.show()
+
 
 
     
 def costGradientReg(thetas, X, y, reg):
-    
     m = np.shape(X)[0]
     h = np.dot(X, thetas)
     y = np.ravel(y)
