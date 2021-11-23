b275623da39186ace8c6ba25091e7ddef4aab969
davidczepiel <48888283+davidczepiel@users.noreply.github.com>
2021-11-23 13:25:45 +0100

    Avances en la practica 5
    Co-Authored-By: nicoFdez <47755232+nicoFdez@users.noreply.github.com>

--- Practica5/Source.py
+++ Practica5/Source.py
@@ -4,10 +4,10 @@ from scipy.io import loadmat
 from matplotlib import pyplot as plt
 from scipy.optimize.zeros import _results_select
 
-LAMBDA = 1
+LAMBDA = 0
 
 def main():
-    parte3()
+    parte4()
 
 
 def parte1():
@@ -25,8 +25,8 @@ def parte1():
     m = np.shape(X)[0]
     X = np.hstack([np.ones([m, 1] ) , X ] )
     n = np.shape(X)[1]
-    thetas = np.zeros(n)
 
+    thetas = np.zeros(n)
     #Le pedimos a minimize que nos calcule las thetas que minimizan el coste por nosotros
     fmin = opt.minimize(fun=costGradientReg, x0=thetas, args=(X, y , 0), 
                         method='TNC', jac=True, options={'maxiter': 70})
@@ -92,39 +92,113 @@ def parte3():
     X = data['X']
     yVal = data['yval']
     xVal = data['Xval']
-    yTest = data['ytest']
-    XTest = data['Xtest']
 
     xMin = np.min(X)
     xMax = np.max(X)
-
     m = np.shape(X)[0]
     n = np.shape(X)[1]
 
-    plt.plot(X, y, "x")
+    originalX = X
 
     #Genero una matriz con las columnas especificadas a partir de las que dispongo
     X = generadorDeColumnas(X,7)
     X , media, desvTipica = normalizeData(X)
     X = np.hstack([np.ones([m, 1] ) , X ] )
 
-    #Le pedimos a minimize que nos calcule las thetas que minimizan el coste por nosotros
 
+    # #Para generar multiples casos entre medias y ver el sobreajuste
+    # thetas = np.zeros(np.shape(X)[1])
+    # fmin = opt.minimize(fun=costGradientReg, x0=thetas, args=(X, y , LAMBDA), 
+    #                     method='TNC', jac=True, options={'maxiter': 70})
+    # thetas = fmin.x
+    # thetasSobreajustadas(thetas, originalX,y,xMin,xMax,media,desvTipica)
+
+
+    # #Vamos a hacer m iteraciones en para calcular 
+    # #los costes de los subgrupos y de los sets de validacion
+    costesEntrenamiento = np.array([])
+    costesValidacion = np.array([])
+    length = range(1,m+1)
+
+    xVal = generadorDeColumnas(xVal,7)
+    xVal = (xVal - media) / desvTipica
+    xVal =  np.hstack([np.ones([np.shape(xVal)[0], 1] ) ,xVal] )
+
+    for i in length:
+        #Obtengo un nuevo set
+        Xentrenar = X[:i,:]
+        Yentrenar = y[:i]
+        #Entreno el algoritmo con dicho set
+        # Xentrenar = np.hstack([np.ones([np.shape(Xentrenar)[0], 1] ) ,Xentrenar] )
+        thetas = np.zeros(np.shape(Xentrenar)[1])
+        fmin = opt.minimize(fun=costGradientReg, x0=thetas, args=(Xentrenar, Yentrenar , LAMBDA), 
+                            method='TNC', jac=True, options={'maxiter': 70})
+        thetas = fmin.x
+
+        #saco los 2 costes, del set y de las X de validacion
+        coste, gradiente = costGradientReg(thetas, Xentrenar, Yentrenar, LAMBDA)
+        costesEntrenamiento = np.append(costesEntrenamiento, coste)
+        coste, gradiente = costGradientReg(thetas, xVal, yVal, LAMBDA)
+        costesValidacion = np.append(costesValidacion, coste)
+
+    plt.plot(range(1,m+1), costesEntrenamiento)
+    plt.plot(range(1,m+1), costesValidacion)
+    plt.show()
+
+
+def parte4():
+        #Sacamos los datos del fichero
+    data = loadmat('ex5data1.mat')
+    y = data['y']
+    X = data['X']
+    yVal = data['yval']
+    xVal = data['Xval']
+
+    m = np.shape(X)[0]
+    costesEntrenamiento = np.array([])
+    costesValidacion = np.array([])
+    lambdas = np.array([ 0, 0.001, 0.003, 0.01, 0.03, 0.1, 0.3, 1, 3, 10])
+
+    #Genero una matriz con las columnas especificadas a partir de las que dispongo
+    X = generadorDeColumnas(X,7)
+    X , media, desvTipica = normalizeData(X)
+    X = np.hstack([np.ones([m, 1] ) , X ] )
+
+    xVal = generadorDeColumnas(xVal,7)
+    xVal = (xVal - media) / desvTipica
+    xVal =  np.hstack([np.ones([np.shape(xVal)[0], 1] ) ,xVal])
+
+    for i in lambdas:
+        thetas = np.zeros(np.shape(X)[1])
+        fmin = opt.minimize(fun=costGradientReg, x0=thetas, args=(X, y , i), 
+                            method='TNC', jac=True, options={'maxiter': 70})
+        thetas = fmin.x
+
+        #saco los 2 costes, del set y de las X de validacion
+        coste, gradiente = costGradientReg(thetas, X, y, i)
+        costesEntrenamiento = np.append(costesEntrenamiento, coste)
+        coste, gradiente = costGradientReg(thetas, xVal, yVal, i)
+        costesValidacion = np.append(costesValidacion, coste)
+
+    plt.plot(lambdas, costesEntrenamiento)
+    plt.plot(lambdas, costesValidacion)
+    plt.show()
+
+
+
+
+def thetasSobreajustadas(thetas, originalX, y, xMin,xMax, media, desvTipica):
+    #Le pedimos a minimize que nos calcule las thetas que minimizan el coste por nosotros
     auxX = np.linspace(xMin,xMax,(int)((xMax-xMin)/0.05))
     auxX = np.reshape(auxX,(np.shape(auxX)[0],1))
     auxX = generadorDeColumnas(auxX,7)
     auxX = (auxX - media) / desvTipica
-    thetas = np.zeros(8)
-
-    fmin = opt.minimize(fun=costGradientReg, x0=thetas, args=(X, y , 0), 
-                        method='TNC', jac=True, options={'maxiter': 70})##MAL
-    thetas = fmin.x
-
+    auxX = np.hstack([np.ones([np.shape(auxX)[0], 1] ) , auxX ] )
+    #Representamos los multiples puntos sobreajustados
     h = np.dot(auxX,thetas)
-    plt.plot(np.linspace(xMin,xMax,(xMax-xMin)/0.05), h, "o")
-
-
-
+    plt.plot(np.linspace(xMin,xMax,(int)((xMax-xMin)/0.05)), h)
+    plt.plot(originalX, y, "x")
+    plt.show()
 
 def generadorDeColumnas(X, p):
     result = X
