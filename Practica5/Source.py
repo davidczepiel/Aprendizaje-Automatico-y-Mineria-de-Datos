0bed7eac935a1aca32c2efd8f5b2956540fc739a
nicoFdez <nicolafe@ucm.es>
2021-11-25 17:17:53 +0100

    Update Source.py
    Practica 5
    
    Co-Authored-By: davidczepiel <48888283+davidczepiel@users.noreply.github.com>

--- Practica5/Source.py
+++ Practica5/Source.py
@@ -7,7 +7,7 @@ from scipy.optimize.zeros import _results_select
 LAMBDA = 0
 
 def main():
-    parte4()
+    parte3()
 
 
 def parte1():
@@ -48,8 +48,6 @@ def parte2():
     X = data['X']
     yVal = data['yval']
     xVal = data['Xval']
-    yTest = data['ytest']
-    XTest = data['Xtest']
 
     #Preparamos los arrays en los que vamos a almacenar los costes 
     m = np.shape(X)[0]
@@ -106,16 +104,16 @@ def parte3():
     X = np.hstack([np.ones([m, 1] ) , X ] )
 
 
-    # #Para generar multiples casos entre medias y ver el sobreajuste
-    # thetas = np.zeros(np.shape(X)[1])
-    # fmin = opt.minimize(fun=costGradientReg, x0=thetas, args=(X, y , LAMBDA), 
-    #                     method='TNC', jac=True, options={'maxiter': 70})
-    # thetas = fmin.x
-    # thetasSobreajustadas(thetas, originalX,y,xMin,xMax,media,desvTipica)
+    #Para generar multiples casos entre medias y ver el sobreajuste
+    thetas = np.zeros(np.shape(X)[1])
+    fmin = opt.minimize(fun=costGradientReg, x0=thetas, args=(X, y , LAMBDA), 
+                        method='TNC', jac=True, options={'maxiter': 70})
+    thetas = fmin.x
+    thetasSobreajustadas(thetas, originalX,y,xMin,xMax,media,desvTipica)
 
 
-    # #Vamos a hacer m iteraciones en para calcular 
-    # #los costes de los subgrupos y de los sets de validacion
+    #Vamos a hacer m iteraciones en para calcular 
+        #los costes de los subgrupos y de los sets de validacion
     costesEntrenamiento = np.array([])
     costesValidacion = np.array([])
     length = range(1,m+1)
@@ -129,16 +127,15 @@ def parte3():
         Xentrenar = X[:i,:]
         Yentrenar = y[:i]
         #Entreno el algoritmo con dicho set
-        # Xentrenar = np.hstack([np.ones([np.shape(Xentrenar)[0], 1] ) ,Xentrenar] )
         thetas = np.zeros(np.shape(Xentrenar)[1])
         fmin = opt.minimize(fun=costGradientReg, x0=thetas, args=(Xentrenar, Yentrenar , LAMBDA), 
-                            method='TNC', jac=True, options={'maxiter': 70})
+                            method='TNC', jac=True, options={'maxiter': 700})
         thetas = fmin.x
 
-        #saco los 2 costes, del set y de las X de validacion
-        coste, gradiente = costGradientReg(thetas, Xentrenar, Yentrenar, LAMBDA)
+        #saco los 2 costes, del set y de las X de validacion y se guardan en un vector para pintarlos
+        coste = cost(thetas, Xentrenar, Yentrenar)
         costesEntrenamiento = np.append(costesEntrenamiento, coste)
-        coste, gradiente = costGradientReg(thetas, xVal, yVal, LAMBDA)
+        coste = cost(thetas, xVal, yVal)
         costesValidacion = np.append(costesValidacion, coste)
 
     plt.plot(range(1,m+1), costesEntrenamiento)
@@ -147,14 +144,15 @@ def parte3():
 
 
 def parte4():
-        #Sacamos los datos del fichero
+    #Sacamos los datos del fichero
     data = loadmat('ex5data1.mat')
     y = data['y']
     X = data['X']
     yVal = data['yval']
     xVal = data['Xval']
-
     m = np.shape(X)[0]
+
+    #Inicializamos los vectores de los costes y los valores de lamba a probar
     costesEntrenamiento = np.array([])
     costesValidacion = np.array([])
     lambdas = np.array([ 0, 0.001, 0.003, 0.01, 0.03, 0.1, 0.3, 1, 3, 10])
@@ -164,26 +162,44 @@ def parte4():
     X , media, desvTipica = normalizeData(X)
     X = np.hstack([np.ones([m, 1] ) , X ] )
 
+    #Aplicamos la misma trnsformacion a los datos de validacion 
     xVal = generadorDeColumnas(xVal,7)
     xVal = (xVal - media) / desvTipica
     xVal =  np.hstack([np.ones([np.shape(xVal)[0], 1] ) ,xVal])
 
+    #Obtenemos el coste para cada lambda
     for i in lambdas:
         thetas = np.zeros(np.shape(X)[1])
         fmin = opt.minimize(fun=costGradientReg, x0=thetas, args=(X, y , i), 
-                            method='TNC', jac=True, options={'maxiter': 70})
+                            method='TNC', jac=True, options={'maxiter': 200})
         thetas = fmin.x
 
         #saco los 2 costes, del set y de las X de validacion
-        coste, gradiente = costGradientReg(thetas, X, y, i)
+        coste = cost(thetas, X, y)
         costesEntrenamiento = np.append(costesEntrenamiento, coste)
-        coste, gradiente = costGradientReg(thetas, xVal, yVal, i)
+        coste = cost(thetas, xVal, yVal)
         costesValidacion = np.append(costesValidacion, coste)
 
     plt.plot(lambdas, costesEntrenamiento)
     plt.plot(lambdas, costesValidacion)
     plt.show()
 
+    ###############COMPROBACION
+    yTest = data['ytest']
+    XTest = data['Xtest']
+    #Aplicamos la misma transformacion a los casos de test
+    XTest = generadorDeColumnas(XTest,7)
+    XTest = (XTest - media) / desvTipica
+    XTest =  np.hstack([np.ones([np.shape(XTest)[0], 1] ) ,XTest])
+    
+    #Sacamos las thetas optimas
+    thetas = np.zeros(np.shape(X)[1])
+    fmin = opt.minimize(fun=costGradientReg, x0=thetas, args=(X, y , 3), 
+                        method='TNC', jac=True, options={'maxiter': 200})
+    thetas = fmin.x
+    #Calculamos el coste
+    coste = cost(thetas, XTest, yTest)
+    print("coste: ", coste)
 
 
 
@@ -234,5 +250,14 @@ def costGradientReg(thetas, X, y, reg):
     gradiente = ((1 / m) * np.dot(np.transpose(X), Aux)) + gradientReg
 
     return coste, gradiente
+
+def cost(thetas, X, y):
+    m = np.shape(X)[0]
+    h = np.dot(X, thetas)
+    y = np.ravel(y)
+    Aux = (h - y) ** 2
+    coste = Aux.sum() / (2 * m)
+    return coste
+
 
 main()
