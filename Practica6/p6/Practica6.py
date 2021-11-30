f6b37f61a7e75522666d72df711c8e38ac664160
davidczepiel <48888283+davidczepiel@users.noreply.github.com>
2021-11-30 17:56:32 +0100

    Parte facil de la practica 6

--- Practica6/p6/Practica6.py
+++ Practica6/p6/Practica6.py
@@ -24,7 +24,7 @@ def parte1():
     svm = SVC(kernel='linear' , C=100.0)
     svm.fit(X, y )
 
-    visualize_boundary(X,y,svm, "parte1C100")
+    mostrar_grafica(X,y,svm)
 
 def parte2():
      #Obtenemos los datos con los que vamos a trabajar
@@ -37,7 +37,7 @@ def parte2():
     #Utilizo la SVM para que me saque una boundary
     svm = SVC(kernel='rbf' , C=C, gamma=1 / ( 2 * sigma**2) )    
     svm.fit(X, y )
-    visualize_boundary(X,y,svm, "parte2")
+    mostrar_grafica(X,y,svm)
 
 
 def parte3():
@@ -52,43 +52,49 @@ def parte3():
     sigma = 0.1
     #Utilizo la SVM para que me saque una boundary
 
-    values = np.array([ 0.01, 0.03, 0.003, 0.1, 0.3, 1, 3, 10, 30])
-    scores = np.zeros((len(values), len(values)))
-    indexes = np.array([0,0])
-    minScore = np.Inf
+    C_vec = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30]
+    sigma_vec = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30]
+    scores = np.zeros((len(C_vec), len(sigma_vec)))
+    best = np.array([0.,0.])
+    minScore = -1
     #saco los aciertos para cada uno de los casos de prueba
-    for i in range(len(values)): #for que recorre las Cs
-        for j in range(len(values)): #for que recorre las gammas
-            svm = SVC(kernel='rbf' , C= values[i], gamma=1 / ( 2 * values[j] **2) )    
+    for i in range(len(C_vec)): #for que recorre las Cs
+        for j in range(len(sigma_vec)): #for que recorre las gammas
+            svm = SVC(kernel='rbf' , C= C_vec[i], gamma=1 / ( 2 * sigma_vec[j] **2) )    
             svm.fit(X, y )
             newScore = svm.score(xVal,yVal)
             scores[i][j] = newScore
-            if newScore <= minScore:
+            #Si he conseguido un mejor porcentage me quedo con ela configuracion
+            if newScore > minScore:
                 minScore = newScore
-                indexes[0]= i
-                indexes[1]= j
+                best[0]= C_vec[i]
+                best[1]= sigma_vec[j]
 
-    svm = SVC(kernel='rbf' , C= values[indexes[0]], gamma=1 / ( 2 * values[indexes[1]] **2) )    
+    svm = SVC(kernel='rbf' , C=  best[0], gamma=1 / ( 2 * best[1] **2) )    
     svm.fit(X, y)
-    visualize_boundary(X,y,svm, "parte3")
+    mostrar_grafica(X,y,svm)
 
 
 
 
-def visualize_boundary(X, y, svm, file_name):
+def mostrar_grafica(X, y, svm):
+    #generamos multiples puntos entre medias del minimo y maximo de los valores
     x1 = np.linspace(X[:, 0].min(), X[:, 0].max(), 100)
     x2 = np.linspace(X[:, 1].min(), X[:, 1].max(), 100)
+
+    #preparamos una grid con los nuevos datos obtenidos
     x1, x2 = np.meshgrid(x1, x2)
     yp = svm.predict(np.array([x1.ravel(), x2.ravel()]).T).reshape(x1.shape)
     pos = (y == 1).ravel()
     neg = (y == 0).ravel()
+
+    #generamos la figura y mostramos los datos 
     plt.figure()
     plt.scatter(X[pos, 0], X[pos, 1], color='black', marker='+')
     plt.scatter(
     X[neg, 0], X[neg, 1], color='yellow', edgecolors='black', marker='o')
     plt.contour(x1, x2, yp)
-    plt.savefig(file_name)
-    plt.close()
+    plt.show()
 
 
 
