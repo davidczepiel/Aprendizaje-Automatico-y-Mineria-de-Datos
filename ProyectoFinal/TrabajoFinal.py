433e0e2752fff535ab4fb0d006b4d05e9825eea5
nicoFdez <nicolafe@ucm.es>
2021-12-23 21:11:27 +0100

    Todo funsiona

--- ProyectoFinal/TrabajoFinal.py
+++ ProyectoFinal/TrabajoFinal.py
@@ -10,10 +10,7 @@ from sklearn.preprocessing import PolynomialFeatures
 
 MAX_LETRAS = 28
 NUM_LETRAS = 3
-LAMBDA = 0.1
 FACTOR_DE_REBAJADO = 0.1
-
-
 
 def main():
     #Sacamos los datos de los ficheros
@@ -48,9 +45,9 @@ def main():
     # XVal,YVal = rebajar_Datos(XVal,YVal,FACTOR_DE_REBAJADO)
     # XTest,YTest = rebajar_Datos(XTest,YTest,FACTOR_DE_REBAJADO)
 
-    #regresion_Logistica(XTrain,YTrain,XVal,YVal,XTest,YTest)
+    #regresion_Logistica(XTrain,YTrain,XVal,YVal,XTest,YTest,X,Y)
     #redes_neuronales(XTrain,YTrain,XVal,YVal,XTest,YTest)
-    SupportVectorMachines(XTrain,YTrain,XVal,YVal,XTest,YTest,X,Y)
+    SupportVectorMachinesMasIteraciones(XTrain,YTrain,XVal,YVal,XTest,YTest,X,Y)
 
 
 
@@ -82,7 +79,7 @@ def rebajar_Datos(X,Y, porcentaje):
 #################################################################
 #####################################REGRESION LOGISCTICA
 ###############################################
-def regresion_Logistica(Xent, Yent, Xval,Yval,XTest,YTest):
+def regresion_Logistica(Xent, Yent, Xval, Yval, XTest, YTest, XWhole, YWhole):
 
     #Les metemos a cada uno de nuestros grupos de datos la columna de 1s 
     #para poder vectorizar comodamente
@@ -92,6 +89,8 @@ def regresion_Logistica(Xent, Yent, Xval,Yval,XTest,YTest):
     Xval = np.hstack([np.ones([m, 1] ) , Xval ])
     m = np.shape(XTest)[0]
     XTest = np.hstack([np.ones([m, 1] ) , XTest ])
+    m = np.shape(XWhole)[0]
+    XWhole = np.hstack([np.ones([m, 1] ) , XWhole ])
 
     #Estos son los valores de lambda con los que vamos a probar
     valoresLambda = np.array([ 0, 0.001, 0.003, 0.01, 0.03, 0.1, 0.3, 1, 3, 10,30])
@@ -102,15 +101,27 @@ def regresion_Logistica(Xent, Yent, Xval,Yval,XTest,YTest):
 
     #En este array se almacenan los porcentajes obtenidos por cada una de las configuraciones
     porcentajesSacados = np.array([])
-
+    porcentajeScores = np.array([0.0,0.0,0.0])
     #Por cada configuracion, entrenamos y validamos con los casos de validacion, si el resultado es mas favorable que el
     #mejor que hemos obtenido nos lo guardamos
     for LambdaActual in valoresLambda:
-        #Obtenemos las thetas que predicen un numero concreto
-        thetasMat = oneVsAll(Xent, Yent, NUM_LETRAS, LambdaActual)
+        for veces in range(3):
+            firstIndex = (int)(np.random.rand(1) * (m*0.35))
+            lastIndex = (int)(firstIndex + (m*0.65))
+            xVal = XWhole[firstIndex:lastIndex,:]
+            yVal = YWhole[firstIndex:lastIndex]
+            randomIndexes = range(firstIndex, lastIndex)
+            X = np.delete(XWhole, randomIndexes, axis=0)
+            y = np.delete(YWhole, randomIndexes)
+    
+            #Obtenemos las thetas que predicen un numero concreto
+            thetasMat = oneVsAll(X, y, NUM_LETRAS, LambdaActual)
+            aciertosActual = calcularAciertos(xVal, yVal, NUM_LETRAS, thetasMat)
+            porcentajeScores[veces] = aciertosActual
+        
         #Con estas thetas vemos cuandos valores que predecimos son iguales al valor real
-        aciertosActual =calcularAciertos(Xval, Yval, NUM_LETRAS, thetasMat)
-        porcentajesSacados = np.append(porcentajesSacados, aciertosActual)
+        porcentajesSacados = np.append(porcentajesSacados, np.sum(porcentajeScores)/3)
+        
         #Si es mas favorable que la mejor configuracion que me he guardado me la guardo
         if aciertosActual > mejorAcierto:
             mejorLambda = LambdaActual
@@ -207,9 +218,162 @@ def gradient(thetas, X, Y):
 #####################################REDES NEURONALES
 ###############################################
 
+def redes_neuronales(XTrain,YTrain,XVal,YVal,XTest,YTest):
+    
+    m = np.shape(XTrain)[0]
+    n = np.shape(XTrain)[1]
+    
+    Y = (YTrain - 1)
+    y_onehot = np.zeros((m, NUM_LETRAS))
+    for i in range(m):
+        y_onehot[i][Y[i]] = 1
+    
+    #Inicializamos las thetas que se usaran en la optimizacion entre -EIni y +EIni
+    EIni = 0.12
+    
+    num_hidden = 50
+    num_entries = n
+    num_labels = NUM_LETRAS
+    
+    auxtheta1 = np.random.rand(num_hidden, n+1) * (2*EIni) - EIni
+    auxtheta2 = np.random.rand(num_labels, num_hidden + 1) * (2*EIni) - EIni
+    
+    params = np.concatenate((np.ravel(auxtheta1), np.ravel(auxtheta2)))
+    
+    #Estos son los valores de lambda con los que vamos a probar
+    valoresLambda = np.array([ 0, 0.001, 0.003, 0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30])
+
+    #en estas variables nos vamos a quedar la configuracion que mejor resultado nos ha dado
+    mejorLambda = 0.01
+    mejorAcierto = 0
+    
+    for lambdaActual in valoresLambda:
+    
+        fmin = opt.minimize(fun=backprop, x0=params, args=(num_entries, num_hidden, num_labels, XTrain, y_onehot, lambdaActual), 
+                            method='TNC', jac=True, options={'maxiter': 300})
+
+        #Se reconstruyen las thetas minimizadas
+        theta1 = np.reshape(fmin.x[:num_hidden * (num_entries + 1)], (num_hidden, (num_entries + 1)))
+        theta2 = np.reshape(fmin.x[num_hidden * (num_entries + 1):], (num_labels, (num_hidden + 1)))
+
+        #Se usa la red neuronal para predecir
+        a1, a2, h = forward_propagate(XVal, theta1, theta2)
+        
+        #Para cada solucion se extrae la probabilidad mas alta de pertenencia
+        prediccion = np.argmax(h, axis=1)
+        prediccion += 1 #Sumamos 1 para desacer la transformacion que se hizo en y_onehot
+        
+        #vector de booleanos donde true indica que el caso i se ha predecido correctamente
+        #ySol = np.ravel(YVal)
+        correctos = prediccion == YVal
+
+        #Calculo de porcentaje de aciertos
+        numCorrectos = (np.sum(correctos) / len(correctos)) * 100
+        
+        print( "Con lambda: ", lambdaActual, " Aciertos: " , numCorrectos , "%")
+        if(numCorrectos > mejorAcierto):
+            mejorAcierto = numCorrectos
+            mejorLambda = lambdaActual
+        
+    #Probamos sobre test nuestra mejor lambda
+    fmin = opt.minimize(fun=backprop, x0=params, args=(num_entries, num_hidden, num_labels, XTrain, y_onehot, mejorLambda), 
+                            method='TNC', jac=True, options={'maxiter': 300})
+
+    #Se reconstruyen las thetas minimizadas
+    theta1 = np.reshape(fmin.x[:num_hidden * (num_entries + 1)], (num_hidden, (num_entries + 1)))
+    theta2 = np.reshape(fmin.x[num_hidden * (num_entries + 1):], (num_labels, (num_hidden + 1)))
+
+    #Se usa la red neuronal para predecir
+    a1, a2, h = forward_propagate(XTest, theta1, theta2)
+    
+    #Para cada solucion se extrae la probabilidad mas alta de pertenencia
+    prediccion = np.argmax(h, axis=1)
+    prediccion += 1 #Sumamos 1 para desacer la transformacion que se hizo en y_onehot
+    
+    #vector de booleanos donde true indica que el caso i se ha predecido correctamente
+    #ySol = np.ravel(YVal)
+    correctos = prediccion == YTest
+    
+    #Calculo de porcentaje de aciertos
+    numCorrectos = (np.sum(correctos) / len(correctos)) * 100
+    
+    print( "Con la mejor lambda: ", mejorLambda, " Aciertos totales: " , numCorrectos , "%")
+
+def backprop(params_rn, num_entries, num_hidden, num_labels, X, y, reg):
+    
+    #Se reconstruyen las thetas que estan como vector en params_rn
+    theta1 = np.reshape(params_rn[:num_hidden * (num_entries + 1)], (num_hidden, (num_entries + 1)))
+    theta2 = np.reshape(params_rn[num_hidden * (num_entries + 1):], (num_labels, (num_hidden + 1)))
+    m = X.shape[0]
+
+    #Se usa propagacion hacia delante 
+    a1, a2, h = forward_propagate(X,theta1, theta2)
+
+    #calculamos el coste para devolverlo
+    cost = costeRedNeuronalRegularizado(h, theta1, theta2, X, y, reg)
+
+    delta1, delta2 = np.zeros(np.shape(theta1)), np.zeros(np.shape(theta2))
+
+    #Para cada caso
+    for t in range(m):
+        a1t = a1[t, :]
+        a2t = a2[t, :]  
+        ht = h[t,:]
+        yt = y[t]
+        #Coste acumulado capa 2-3
+        d3t = ht-yt
+        #Coste acumulado capa 1-2
+        d2t = np.dot(theta2.T, d3t) * (a2t * (1 - a2t))
+        #Gradiente
+        delta1 = delta1 + np.dot(d2t[1:, np.newaxis], a1t[np.newaxis, :])
+        delta2 = delta2 + np.dot(d3t[:, np.newaxis], a2t[np.newaxis, :])
+ 
+    #Gradiente sin regularizar
+    gradient1 = delta1 / m
+    gradient2 = delta2 / m
+
+    #Regularizacion del gradiente
+    reg1 = (reg / m) * theta1
+    reg2 = (reg / m) * theta2
+    
+    reg1[:, 0] = 0
+    reg2[:, 0] = 0
+    
+    gradient1 += reg1
+    gradient2 += reg2
+
+    #Se convierte el gradiente enun vector
+    gradient = np.concatenate((np.ravel(gradient1), np.ravel(gradient2)))
+
+    return cost, gradient
+
+def forward_propagate(X, Theta1, Theta2):
+    m = X.shape[0]
+    #Salida capa de entrada
+    A1 = np.hstack([np.ones([m, 1]), X])
+    #Salida capa oculta
+    Z2 = np.dot(A1, Theta1.T)
+    A2 = np.hstack([np.ones([m, 1]), sigmoid(Z2)])
+    #Salida red neuronal
+    Z3 = np.dot(A2, Theta2.T)
+    #Convertir la salida de la red neuronal en probabilidades
+    H = sigmoid(Z3)
+    return A1, A2, H
+
+def sigmoid(z):
+    return 1/(1+(np.e**(-z)))
 
 
+##############################################COST####################################
+def costeRedNeuronalRegularizado(h, theta1, theta2, X, Y, reg):
+    m = len(X)
+    extra = (reg/(2*m)) * (np.sum(theta1[:,1:]**2) + np.sum(theta2[:,1:]**2))
+    return costeRedNeuronal(h, X, Y) + extra
 
+
+def costeRedNeuronal(h, X, Y):
+    m = X.shape[0]
+    return (-1/m) * (np.sum(Y * np.log(h) + (1-Y) * np.log(1-h)))
 
 ##############################################
 #####################################REDES NEURONALES
@@ -225,54 +389,11 @@ def gradient(thetas, X, Y):
 #####################################SUPPORT VECTOR MACHINES 
 ###############################################
 
-def SupportVectorMachines(X,y,xVal,yVal,xTest,yTest,XWhole, YWhole ):
-
-    #Estas son todas las configuraciones que vamos a probar para las SVM 
-    C_vec = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30]
-    sigma_vec = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30]
-    #En scores vamos a almacenar las puntuaciones que nos den cada configuracion
-    scores = np.zeros((len(C_vec), len(sigma_vec)))
-    #En best nos vamos a guardar la mejor configuracion hasta el momento
-    best = np.array([0.,0.])
-    #maxScore nos permite comprobar si la configuracion actual es mejor que la 
-    #que teniamos guardada como mas optima
-    maxScore = -1
-
-    #saco los aciertos para cada uno de los casos de prueba
-    for i in range(len(C_vec)): #for que recorre las Cs
-        for j in range(len(sigma_vec)): #for que recorre las gammas
-            print("\nComprobando la config [", C_vec[i],",",sigma_vec[j],"]")
-            #Entreno la SVM con los casos de entrenamiento y saco la puntuacion que obtiene sobre los casos
-            #de validacion
-            svm = SVC(kernel='rbf' , C= C_vec[i], gamma=1 / ( 2 * sigma_vec[j] **2)) 
-            svm.fit(X, y )
-
-            newScore = svm.score(xVal,yVal)
-            scores[i][j] = newScore
-            #Si he conseguido un mejor porcentage me quedo con esta configuracion
-            if newScore > maxScore:
-                maxScore = newScore
-                best[0]= C_vec[i]
-                best[1]= sigma_vec[j]
-                print("Nueva max Score: ", maxScore, "con la configuracion ",best)
-
-    #Entreno para la mejor de las configuraciones y saco el score de adivinar con los casos de test
-    svm = SVC(kernel='rbf' , C=  best[0], gamma=1 / ( 2 * best[1] **2) )    
-    svm.fit(X, y)
-    
-    newScore = svm.score(xTest,yTest)
-    print("//////////////////SVM///////////////////////////")
-    print("Porcentaje de aciertos de alfabeto arabe: ", newScore*100 , "%")
-    print("//////////////////SVM///////////////////////////")
-
-
-
-
 def SupportVectorMachinesMasIteraciones(X,y,xVal,yVal,xTest,yTest,XWhole, YWhole ):
 
     #Estas son todas las configuraciones que vamos a probar para las SVM 
-    C_vec = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30]
-    sigma_vec = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30]
+    C_vec = [0.01, 0.03, 0.05, 0.1, 0.3, 1, 3, 5, 10, 20, 30, 40, 50, 60]
+    sigma_vec = [1, 3, 5, 10, 20, 30, 40, 50, 60, 65, 70, 75, 80 , 85, 90, 140]
     #En scores vamos a almacenar las puntuaciones que nos den cada configuracion
     scores = np.zeros((len(C_vec), len(sigma_vec)))
     #En best nos vamos a guardar la mejor configuracion hasta el momento
@@ -290,14 +411,14 @@ def SupportVectorMachinesMasIteraciones(X,y,xVal,yVal,xTest,yTest,XWhole, YWhole
         for j in range(len(sigma_vec)): #for que recorre las gammas
             print("\nComprobando la config [", C_vec[i],",",sigma_vec[j],"]")
             for veces in range(3):
-                randomIndexes = np.random.rand((int)(m*0.3)) * m
-                randomIndexes = randomIndexes.astype(int)
-                xVal = XWhole[randomIndexes]
-                yVal = YWhole[randomIndexes]
+                firstIndex = (int)(np.random.rand(1) * (m*0.35))
+                lastIndex = (int)(firstIndex + (m*0.65))
+                xVal = XWhole[firstIndex:lastIndex,:]
+                yVal = YWhole[firstIndex:lastIndex]
+                randomIndexes = range(firstIndex, lastIndex)
                 X = np.delete(XWhole, randomIndexes, axis=0)
                 y = np.delete(YWhole, randomIndexes)
-
-                print("La A sale ", np.count_nonzero(y == 1), " veces la B ", np.count_nonzero(y == 2)," veces la C ", np.count_nonzero(y == 3)  )
+            
                 #Entreno la SVM con los casos de entrenamiento y saco la puntuacion que obtiene sobre los casos
                 #de validacion
                 svm = SVC(kernel='rbf' , C= C_vec[i], gamma=1 / ( 2 * sigma_vec[j] **2)) 
@@ -305,7 +426,7 @@ def SupportVectorMachinesMasIteraciones(X,y,xVal,yVal,xTest,yTest,XWhole, YWhole
                 porcentajeScores[veces] = svm.score(xVal,yVal)
             print(porcentajeScores)
 
-            newScore = svm.score(xVal,yVal)
+            newScore = svm.score(xVal,yVal) #NO SIRVE??
             newScore = np.sum(porcentajeScores)/3.0
             scores[i][j] = newScore
             #Si he conseguido un mejor porcentage me quedo con esta configuracion
@@ -314,6 +435,11 @@ def SupportVectorMachinesMasIteraciones(X,y,xVal,yVal,xTest,yTest,XWhole, YWhole
                 best[0]= C_vec[i]
                 best[1]= sigma_vec[j]
                 print("Nueva max Score: ", maxScore, "con la configuracion ",best)
+                A = np.count_nonzero(y == 1)
+                B = np.count_nonzero(y == 2)
+                C = np.count_nonzero(y == 3)
+                print("La A sale ", A, " veces la B ", B," veces la C ", C )
+                
 
     #Entreno para la mejor de las configuraciones y saco el score de adivinar con los casos de test
     svm = SVC(kernel='rbf' , C=  best[0], gamma=1 / ( 2 * best[1] **2) )    
